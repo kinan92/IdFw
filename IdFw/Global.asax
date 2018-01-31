ccf541fd6cb0f970596f2ab9128c3d60ba0d7c65
kinan.karam92 <kinan.karam92@gmail.com>
2018-01-31 09:06:30 +0100

    Add project files.

---
+++ IdFw/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IdFw.MvcApplication" Language="C#" %>
---
+++ IdFw/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdFw.Startup))]
+namespace IdFw
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
